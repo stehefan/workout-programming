1c57f025af0882e7f3a9d8238439bf028ac7085b
Stefan Lier <lier.stefan@gmail.com>
2024-10-04 13:45:23 +0200

    feat: use preview images for each exercise and loose the tabular design

--- app/components/Exercise/Exercise.tsx
+++ app/components/Exercise/Exercise.tsx
@@ -2,6 +2,7 @@
 
 import ReactPlayer from 'react-player/lazy'
 import React, {useState} from "react";
+import Image from "next/image";
 
 export default function Exercise() {
     const [display, setDisplay] = useState(false);
@@ -10,22 +11,30 @@ export default function Exercise() {
     const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setTextArea(event.target.value);
     };
-
+// Photo by <a href="https://unsplash.com/@srosinger3997?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Samantha Gades</a> on <a href="https://unsplash.com/photos/woman-holding-dumbbell-in-white-crew-neck-t-shirt-k95uqdEe8R4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
     return (
-        <div className={'bg-slate-100 dark:bg-neutral-800 p-2 w-96 grid grid-cols-[30%_70%] gap-2 rounded-lg'}>
-            <div className='font-bold'>Name</div>
-            <div className='font-thin'>Pelvis Tilts</div>
-            <div className='font-bold'>Repetitions</div>
-            <div className='font-thin'>⊘</div>
-            <div className='font-bold'>Notes</div>
-            <div className='pr-2'>
-                <textarea className='resize-none w-full p-2 bg-slate-100 dark:bg-neutral-800 border border-neutral-800 dark:border-slate-100 dark:text-slate-100 text-neutral-800' value={textArea} onChange={handleChange} rows={5}></textarea>
-            </div>
-            <div className='font-bold'>
-                Video
+        <div className={'w-96 flex flex-col rounded-lg border border-neutral-700'}>
+            <div className={'relative aspect-video rounded-t-lg cursor-pointer'}>
+                <Image
+                    src={'https://images.unsplash.com/photo-1537289150563-b7f10eee353b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxMjA3fDB8MXxhbGx8MTE5fHx8fHx8fHwxNzI4MDM4MjU0fA&ixlib=rb-4.0.3&q=80&w=1080'}
+                    alt={'Woman holding dumbbell in white crew neck t-shirt'}
+                    width={1080}
+                    height={721}
+                    onClick={() => setDisplay(!display)}
+                    className={'rounded-t-lg object-cover w-96 h-52'}/>
+                <span
+                    className='w-16 h-16 text-5xl absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 text-white inline-flex justify-center items-center rounded-full bg-neutral-800/75'
+                >
+                    &#9205;
+                </span>
             </div>
             <div>
-                <button onClick={() => setDisplay(!display)}>show</button>
+
+                <div>Pelvis Tilts</div>
+                <div>7 repetitions</div>
+                <textarea
+                    className='resize-none w-full p-2 bg-slate-100 dark:bg-neutral-800 border border-neutral-800 dark:border-slate-100 dark:text-slate-100 text-neutral-800'
+                    value={textArea} onChange={handleChange} rows={5}></textarea>
             </div>
             {display && (
                 <div
