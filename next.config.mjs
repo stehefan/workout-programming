f977bd525a21e36f859579ee297bd4d8cd156c3e
Stefan Lier <lier.stefan@gmail.com>
2024-10-05 12:32:00 +0200

    feat: configure next to use pictures stored in vercel blob storage

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'picsum.photos',
+                hostname: '*.public.blob.vercel-storage.com',
                 port: '',
             },
         ],
