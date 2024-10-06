5fd374fc7f352dd2ef0817d2ca28006b76f23441
Stefan Lier <lier.stefan@gmail.com>
2024-10-06 17:28:28 +0200

    fix: adjust css and html to position description and form-content properly

--- app/components/Exercise/Exercise.tsx
+++ app/components/Exercise/Exercise.tsx
@@ -40,7 +40,8 @@ export default function Exercise(props: ExerciseProps) {
 
     return (
         <div className={'w-96 flex flex-col rounded-lg border border-neutral-300 dark:border-neutral-600'}>
-            <div className={'relative aspect-video rounded-t-lg cursor-pointer'} onClick={() => setDisplayVideoPlayer(!displayVideoPlayer)}>
+            <div className={'relative aspect-video rounded-t-lg cursor-pointer'}
+                 onClick={() => setDisplayVideoPlayer(!displayVideoPlayer)}>
                 {hasPreviewImage ? (
                     <Image
                         src={props.exercise.previewImageUrl!}
@@ -57,24 +58,24 @@ export default function Exercise(props: ExerciseProps) {
                     <span
                         className='w-16 h-16 text-5xl absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 text-white inline-flex justify-center items-center rounded-full bg-neutral-800/75'
                     >
-                        <PlayIcon className='size-6' />
+                        <PlayIcon className='size-6'/>
                     </span>
                 )}
             </div>
-            <form action={handleSave} onSubmit={() => setIsSaving(true)} className={'h-full'}>
-                <div className='flex flex-col gap-2 p-2 h-full justify-between relative'>
-                    <div className='mb-4'>
-                        <div className='font-bold text-xl '>
-                            {props.exercise.exerciseName}
-                        </div>
-                        <div className='inline-flex text-xs items-center'>
-                            {props.exercise.measureCount} {props.exercise.measureUnit}
-                        </div>
+            <div className='flex flex-col gap-2 p-2 h-full justify-between'>
+                <div className='mb-4 w-full'>
+                    <div className='font-bold text-xl '>
+                        {props.exercise.exerciseName}
                     </div>
+                    <div className='inline-flex text-xs items-center'>
+                        {props.exercise.measureCount} {props.exercise.measureUnit}
+                    </div>
+                </div>
+                <form action={handleSave} onSubmit={() => setIsSaving(true)} className='w-full'>
                     <input type={'hidden'} name={'id'} value={props.exercise.id}/>
                     <textarea
                         name={'note'}
-                        className='resize-none p-2 bg-neutral-300 dark:bg-neutral-900 dark:text-neutral-300 text-neutral-600 focus:outline-none rounded-b-lg'
+                        className='resize-none w-full p-2 bg-neutral-300 dark:bg-neutral-900 dark:text-neutral-300 text-neutral-600 focus:outline-none rounded-b-lg'
                         value={note}
                         onChange={handleChange}
                         placeholder='Your notes'
@@ -85,14 +86,14 @@ export default function Exercise(props: ExerciseProps) {
                             disabled={isSaving || !saveIndicatorIsEnabled}
                             className={`w-full flex items-center justify-center bg-neutral-300 dark:bg-neutral-900 dark:text-neutral-300 text-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-300`}>
                         {isSaving ? (
-                            <ArrowPathIcon className='size-8 inline p-2 animate-spin' />
+                            <ArrowPathIcon className='size-8 inline p-2 animate-spin'/>
                         ) : (
-                            <CloudArrowDownIcon className="size-8 inline p-2" />
+                            <CloudArrowDownIcon className="size-8 inline p-2"/>
                         )}
                         Save
                     </button>
-                </div>
-            </form>
+                </form>
+            </div>
             {
                 displayVideoPlayer && hasVideo && (
                     <VideoPlayer
