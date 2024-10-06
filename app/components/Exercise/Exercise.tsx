4cfe6a61317de184b6e90599104c83349cec3f0f
Stefan Lier <lier.stefan@gmail.com>
2024-10-06 17:18:15 +0200

    feat: use heroicons react package to make the JSX more readable

--- app/components/Exercise/Exercise.tsx
+++ app/components/Exercise/Exercise.tsx
@@ -6,6 +6,7 @@ import VideoPlayer from "@/app/components/VideoPlayer/VideoPlayer";
 import {ExerciseEntry} from "@/types/Exercise";
 import PreviewImagePlaceholder from "@/app/components/Exercise/PreviewImagePlaceholder";
 import {updateNote} from "@/app/actions";
+import {ArrowPathIcon, CloudArrowDownIcon, PlayIcon} from "@heroicons/react/24/solid";
 
 export type ExerciseProps = {
     exercise: ExerciseEntry;
@@ -56,12 +57,7 @@ export default function Exercise(props: ExerciseProps) {
                     <span
                         className='w-16 h-16 text-5xl absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 text-white inline-flex justify-center items-center rounded-full bg-neutral-800/75'
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                             className="size-6">
-                            <path fillRule="evenodd"
-                                  d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-                                  clipRule="evenodd"/>
-                        </svg>
+                        <PlayIcon className='size-6' />
                     </span>
                 )}
             </div>
@@ -89,19 +85,9 @@ export default function Exercise(props: ExerciseProps) {
                             disabled={isSaving || !saveIndicatorIsEnabled}
                             className={`w-full flex items-center justify-center bg-neutral-300 dark:bg-neutral-900 dark:text-neutral-300 text-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-300`}>
                         {isSaving ? (
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                 className="size-8 inline p-2 animate-spin">
-                                <path fillRule="evenodd"
-                                      d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z"
-                                      clipRule="evenodd"/>
-                            </svg>
+                            <ArrowPathIcon className='size-8 inline p-2 animate-spin' />
                         ) : (
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                 className="size-8 inline p-2">
-                                <path fillRule="evenodd"
-                                      d="M10.5 3.75a6 6 0 0 0-5.98 6.496A5.25 5.25 0 0 0 6.75 20.25H18a4.5 4.5 0 0 0 2.206-8.423 3.75 3.75 0 0 0-4.133-4.303A6.001 6.001 0 0 0 10.5 3.75Zm2.03 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v4.94a.75.75 0 0 0 1.5 0v-4.94l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
-                                      clipRule="evenodd"/>
-                            </svg>
+                            <CloudArrowDownIcon className="size-8 inline p-2" />
                         )}
                         Save
                     </button>
