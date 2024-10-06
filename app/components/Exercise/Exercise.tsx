f721b985a722a95c1ad3effa6a85cd22bb471fbf
Stefan Lier <lier.stefan@gmail.com>
2024-10-06 16:41:22 +0200

    feat: simplify triggering and displaying save-status

--- app/components/Exercise/Exercise.tsx
+++ app/components/Exercise/Exercise.tsx
@@ -87,13 +87,23 @@ export default function Exercise(props: ExerciseProps) {
                     <button type='submit' aria-label='Update note of exercise'
                             aria-disabled={isSaving || !saveIndicatorIsEnabled}
                             disabled={isSaving || !saveIndicatorIsEnabled}
-                            className={`absolute right-5 bottom-5 disabled:cursor-not-allowed transition-all duration-500 ease-linear ${saveIndicatorIsEnabled ? 'opacity-100' : 'opacity-25 '} ${isSaving ? 'motion-safe:animate-pulse' : ''}`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                             className={`size-10`}>
-                            <path fillRule="evenodd"
-                                  d="M10.5 3.75a6 6 0 0 0-5.98 6.496A5.25 5.25 0 0 0 6.75 20.25H18a4.5 4.5 0 0 0 2.206-8.423 3.75 3.75 0 0 0-4.133-4.303A6.001 6.001 0 0 0 10.5 3.75Zm2.03 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v4.94a.75.75 0 0 0 1.5 0v-4.94l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
-                                  clipRule="evenodd"/>
-                        </svg>
+                            className={`w-full flex items-center justify-center bg-neutral-300 dark:bg-neutral-900 dark:text-neutral-300 text-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-300`}>
+                        {isSaving ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                 className="size-8 inline p-2 animate-spin">
+                                <path fillRule="evenodd"
+                                      d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z"
+                                      clipRule="evenodd"/>
+                            </svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                 className="size-8 inline p-2">
+                                <path fillRule="evenodd"
+                                      d="M10.5 3.75a6 6 0 0 0-5.98 6.496A5.25 5.25 0 0 0 6.75 20.25H18a4.5 4.5 0 0 0 2.206-8.423 3.75 3.75 0 0 0-4.133-4.303A6.001 6.001 0 0 0 10.5 3.75Zm2.03 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v4.94a.75.75 0 0 0 1.5 0v-4.94l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
+                                      clipRule="evenodd"/>
+                            </svg>
+                        )}
+                        Save
                     </button>
                 </div>
             </form>
