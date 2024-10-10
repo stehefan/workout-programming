1bcd6f5d034c2077e0c73d9d4a830d39c9a2594b
Stefan Lier <lier.stefan@gmail.com>
2024-10-10 13:31:20 +0200

    fix: define correct base-width based on container-width

--- app/workout/[workoutId]/loading.tsx
+++ app/workout/[workoutId]/loading.tsx
@@ -11,7 +11,7 @@ const Loading = () => {
                     <div className="h-4 w-1/4 bg-neutral-700 rounded animate-pulse mb-4"></div>
                     <div className="grid grid-cols-[repeat(auto-fill,_minmax(20rem,_1fr))] justify-items-center gap-4">
                         {[...Array(3)].map((_, exerciseIndex) => (
-                            <div key={exerciseIndex} className='h-[32rem] border border-neutral-700 animate-pulse rounded-lg p-4'>
+                            <div key={exerciseIndex} className='h-[32rem] w-full max-w-96 border border-neutral-700 animate-pulse rounded-lg p-4'>
                                 <div key={exerciseIndex} className="h-40 bg-neutral-700 rounded animate-pulse"></div>
                                 <div className="h-4 w-1/2 bg-neutral-700 rounded animate-pulse mt-2"></div>
                                 <div className="h-2 w-1/3 bg-neutral-700 rounded animate-pulse mt-2"></div>
