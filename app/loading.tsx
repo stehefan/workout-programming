e15afa4c6037d4d72754a394a0ece14c0db942c3
Stefan Lier <lier.stefan@gmail.com>
2024-12-02 20:59:13 +0100

    fix: align workout-skeleton on start of flex-container

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="h-full flex flex-col justify-center p-4 space-y-4">
+    <div className="h-full flex flex-col justify-start p-4 space-y-4">
       <div className="animate-pulse bg-neutral-700 h-10 w-2/3 rounded-2xl"></div>
       <div className="grid grid-cols-2 max-md:grid-cols-1 gap-4">
         {Array.from({ length: 2 }).map((_, i) => (
