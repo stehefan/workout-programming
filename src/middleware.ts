e00dcaa8c33ec071d36b614d02022d8bceb20082
Stefan Lier <lier.stefan@gmail.com>
2024-12-07 10:22:44 +0100

    fix: add apple-icon route to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import {clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/icon(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/(apple-)?icon(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {
