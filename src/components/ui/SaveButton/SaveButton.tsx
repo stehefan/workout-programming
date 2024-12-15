5446fb0c5db028a0c28fb5869e50f57d75a0f91a
Stefan Lier <lier.stefan@gmail.com>
2024-12-15 15:12:17 +0100

    feat: allow save-button to be disabled

--- src/components/ui/SaveButton/SaveButton.tsx
+++ src/components/ui/SaveButton/SaveButton.tsx
@@ -4,11 +4,12 @@ export type CancelButtonProps = {
     submitFn: (data: FormData) => void;
     isSaving: boolean;
     additionalClassNames?: string;
+    disabled?: boolean;
 }
 
-export function SaveButton({submitFn, isSaving, additionalClassNames = ''}: CancelButtonProps) {
+export function SaveButton({submitFn, isSaving, additionalClassNames = '', disabled = false}: CancelButtonProps) {
     return (
-        <button formAction={submitFn}
+        <button formAction={submitFn} disabled={disabled}
                 className={`bg-green-800 inline-flex items-center justify-center ${additionalClassNames}`}>
             {isSaving ? (
                 <ArrowPathIcon className='size-5 mr-2 animate-spin'/>
