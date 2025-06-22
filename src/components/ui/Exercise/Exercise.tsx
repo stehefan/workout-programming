df2feff32c8e9084139e7ff1c958c36b4e959640
Stefan Lier <lier.stefan@gmail.com>
2025-06-22 16:29:30 +0200

    fix: make play-button more a11y friendly

--- src/components/ui/Exercise/Exercise.tsx
+++ src/components/ui/Exercise/Exercise.tsx
@@ -14,14 +14,14 @@ export type ExerciseProps = {
     exercise: ExerciseEntry;
 }
 
-export default function Exercise(props: ExerciseProps) {
+export default function Exercise({ exercise }: ExerciseProps) {
     const [displayVideoPlayer, setDisplayVideoPlayer] = useState(false);
-    const [note, setNote] = useState(props.exercise.note);
+    const [note, setNote] = useState(exercise.note);
     const [isSaving, setIsSaving] = useState(false)
 
-    const hasVideo = props.exercise.videoUrl !== undefined;
-    const hasPreviewImage = props.exercise.image !== undefined;
-    const saveIndicatorIsEnabled = note !== props.exercise.note;
+    const hasVideo = exercise.videoUrl !== undefined;
+    const hasPreviewImage = exercise.image !== undefined;
+    const saveIndicatorIsEnabled = note !== exercise.note;
 
     const handleSave = async (formData: FormData) => {
         try {
@@ -40,21 +40,23 @@ export default function Exercise(props: ExerciseProps) {
     const width: number = 382;
     const height: number = Math.floor(width / 16 * 9);
 
+    const playVideoTitle = `Play video for ${exercise.exerciseName}`;
+
     return (
         <div
             className={'w-full max-w-96 flex flex-col rounded-lg border border-neutral-300 dark:border-neutral-600 relative'}>
             <Link
-                href={`/exercise/${props.exercise.id}`}
+                href={`/exercise/${exercise.id}`}
                 className='inline-flex items-center size-10 justify-center text-sm absolute z-10 right-0 top-0 m-2 bg-black/40 rounded-md cursor-pointer focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground'>
                 <span className='size-5'>
                     <PencilSquareIcon />
                 </span>
                 <span className="sr-only">Edit item</span>
             </Link>
-            <div className={'relative rounded-t-lg'}>
+            <div className='relative rounded-t-lg'>
                 {hasPreviewImage ? (
                     <Image
-                        src={`https://wsrv.nl/?url=${props.exercise.image!.imageUrl}&w=${width}&h=${height}&dpr=2`}
+                        src={`https://wsrv.nl/?url=${exercise.image!.imageUrl}&w=${width}&h=${height}&dpr=2`}
                         alt={'Woman holding dumbbell in white crew neck t-shirt'}
                         width={width}
                         height={height}
@@ -64,26 +66,28 @@ export default function Exercise(props: ExerciseProps) {
                         <PreviewImagePlaceholder />
                     </div>
                 )}
-                {props.exercise.videoUrl && (
-                    <span
-                        className='w-16 h-16 text-5xl absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 text-white inline-flex justify-center items-center rounded-full bg-neutral-800/75 cursor-pointer'
+                {hasVideo && (
+                    <button
+                        title={playVideoTitle}
+                        aria-label={playVideoTitle}
+                        className='w-16 h-16 text-5xl cursor-pointer absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 text-white inline-flex justify-center items-center rounded-full! border-none! bg-neutral-800/75!'
                         onClick={() => setDisplayVideoPlayer(!displayVideoPlayer)}
                     >
                         <PlayIcon className='size-6' />
-                    </span>
+                    </button>
                 )}
             </div>
             <div className='flex flex-col gap-2 p-2 h-full justify-between'>
                 <div className='mb-4 w-full'>
                     <div className='font-bold text-xl '>
-                        {props.exercise.exerciseName}
+                        {exercise.exerciseName}
                     </div>
                     <div className='inline-flex text-xs items-center'>
-                        {props.exercise.measureCount} {props.exercise.measureUnit}
+                        {exercise.measureCount} {exercise.measureUnit}
                     </div>
                 </div>
                 <form action={handleSave} onSubmit={() => setIsSaving(true)} className='w-full'>
-                    <input type={'hidden'} name={'id'} value={props.exercise.id} />
+                    <input type={'hidden'} name={'id'} value={exercise.id} />
                     <textarea
                         name={'note'}
                         className='resize-none w-full p-2 bg-neutral-300 dark:bg-neutral-900 dark:text-neutral-300 text-neutral-600 focus:outline-hidden rounded-b-lg'
@@ -108,7 +112,7 @@ export default function Exercise(props: ExerciseProps) {
             {
                 displayVideoPlayer && hasVideo && (
                     <VideoPlayer
-                        videoUrl={props.exercise.videoUrl!}
+                        videoUrl={exercise.videoUrl!}
                         closeFn={() => setDisplayVideoPlayer(false)}
                     />
                 )}
